7ea153f24292b02b9efefe0b49628f1067430e66
inman <sf990668852@gmail.com>
2022-03-24 11:54:29 +0800

    update

--- src/component/Solar.js
+++ src/component/Solar.js
@@ -1,63 +1,31 @@
 import React from "react";
 
-import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 
-import rwdConfig from "../config/rwdConfig";
+import solar from "../img/solar.jpg";
 
-const TitleContainer = styled.div`
-  width: 100%;
-  height: 10%;
-  // border: 1px red solid;
-`;
-
-const Title = styled.div`
-  margin: 2rem;
-  color: #fff;
-  font-size: 2rem;
-`;
-
-const LowerContainer = styled.div`
-  width: 100%;
-  height: 85%;
-  display: flex;
-  flex-direction: row;
-  border: 1px red solid;
-  @media ${rwdConfig.tablet} {
-    flex-direction:column;
-  }
-}
-`;
-
-const HouseImg = styled.div`
-  height: 100%;
-  width: 60%;
-  border: 1px yellow solid;
-  @media ${rwdConfig.tablet} {
-    height: 55%;
-    width:100%;
-  }
-`;
-
-const IndexContainer = styled.div`
-  height: 100%;
-  width: 40%;
-  border: 1px green solid;
-  @media ${rwdConfig.tablet} {
-    height: 45%;
-    width:100%;
-  }
-`;
+import {
+  TitleContainer,
+  Title,
+  LowerContainer,
+  HouseImg,
+  IndexContainer,
+  GobackBtn,
+} from "./style/style.js";
 
 const Solar = () => {
+  const navigate = useNavigate();
+
   return (
     <>
       <TitleContainer>
         <Title>⬜️ 太陽能發電效率</Title>
       </TitleContainer>
       <LowerContainer>
-        <HouseImg></HouseImg>
+        <HouseImg style={{ backgroundImage: `url(${solar})` }} />
         <IndexContainer></IndexContainer>
       </LowerContainer>
+      <GobackBtn onClick={() => navigate("/")}>返 回</GobackBtn>
     </>
   );
 };
---
+++ src/component/style/style.js
@@ -0,0 +1,64 @@
+import styled from "styled-components";
+
+import rwdConfig from "../../config/rwdConfig";
+
+const TitleContainer = styled.div`
+  width: 100%;
+  height: 10%;
+  // border: 1px red solid;
+`;
+
+const Title = styled.div`
+  margin: 2rem;
+  color: #fff;
+  font-size: 2rem;
+`;
+
+const LowerContainer = styled.div`
+  width: 100%;
+  height: 85%;
+  display: flex;
+  flex-direction: row;
+  border: 1px red solid;
+  @media ${rwdConfig.tablet} {
+    flex-direction:column;
+  }
+}
+`;
+
+const HouseImg = styled.div`
+  height: 100%;
+  width: 60%;
+  border: 1px yellow solid;
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position: center;
+  @media ${rwdConfig.tablet} {
+    height: 55%;
+    width: 100%;
+  }
+`;
+
+const IndexContainer = styled.div`
+  height: 100%;
+  width: 40%;
+  border: 1px green solid;
+  @media ${rwdConfig.tablet} {
+    height: 45%;
+    width: 100%;
+  }
+`;
+
+const GobackBtn = styled.div`
+  position: fixed;
+  width: 4rem;
+  padding: 0.4rem 0.8rem;
+  bottom: 2rem;
+  left: 2rem;
+  text-align: center;
+  color: #fff;
+  background-color: #38665c;
+  border-radius: 0.5rem;
+`;
+
+export {TitleContainer, Title, LowerContainer, HouseImg, IndexContainer, GobackBtn};
